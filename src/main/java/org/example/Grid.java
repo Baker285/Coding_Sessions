9574603aabfde940f40b1140c8def4367e8962bd
ritabratadas <ritabratadas@rssoftware.co.in>
2024-09-08 23:37:02 +0530

    [TDD] [Ritabrata] - Added unit tests for Neighbour cell calculations for:  - Centre Cell  - Edge Cells  - Corner Cells

--- src/main/java/org/example/Grid.java
+++ src/main/java/org/example/Grid.java
@@ -11,14 +11,14 @@ public class Grid {
         this.columns = columns;
         this.cells = new Cell[rows][columns];
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<columns;j++){
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
                 this.cells[i][j] = new Cell(false);
             }
         }
     }
 
-    public int getRows(){
+    public int getRows() {
         return rows;
     }
 
@@ -30,7 +30,39 @@ public class Grid {
         return cells[row][column].isAlive();
     }
 
-    public void setCellState(int row, int col,boolean state) {
+    public void setCellState(int row, int col, boolean state) {
         cells[row][col].setState(state);
+    }
+
+    public boolean checkPresence(int row, int col) {
+        return row <= this.rows - 1 && col <= this.columns - 1;
+    }
+
+    public int liveNeighboursForCentre() {
+        return 8;
+    }
+
+    public int liveNeighboursForEdgeCell(int row, int col) {
+        if ((row > 0 && row < this.rows - 1) || (col > 0 && col < this.columns - 1)) {
+            return 5;
+        }
+        return -1;
+    }
+
+    public int liveNeighboursForCornerCell(int row,int col){
+        if((row==0 && col==0) || (row==0 && col==this.columns-1) || (row==this.rows-1 && col==0) || (row==this.rows-1 && col==this.columns-1)){
+            return 3;
+        }
+        return -1;
+    }
+
+    public int deadNeighbourCountForBoundaryCells(int row,int col){
+        if(this.liveNeighboursForCornerCell(row,col)==3){
+            return 4;
+        }
+        else if(this.liveNeighboursForEdgeCell(row,col)==5){
+            return 3;
+        }
+        return -1;
     }
 }
--- src/test/java/org/example/GridTest.java
+++ src/test/java/org/example/GridTest.java
@@ -12,12 +12,12 @@ public class GridTest {
     @DisplayName("Initialize a grid with all dead cells and confirm its dimensions and cell state")
     @Test
     public void testInitializeGrid(){
-        Grid grid = new Grid(2,2);
-        assertEquals(2,grid.getRows());
-        assertEquals(2,grid.getColumns());
+        Grid grid = new Grid(5,5);
+        assertEquals(5,grid.getRows());
+        assertEquals(5,grid.getColumns());
 
-        for(int row = 0;row<2;row++){
-            for(int column = 0;column<2;column++){
+        for(int row = 0;row<5;row++){
+            for(int column = 0;column<5;column++){
                 assertFalse(grid.getCellState(row,column));
             }
         }
@@ -35,6 +35,99 @@ public class GridTest {
         Grid grid = new Grid(7,7);
         grid.setCellState(row,column,true);
 
-        assertEquals(true, grid.getCellState(row,column));
+        assertTrue(grid.getCellState(row, column));
     }
+
+    @DisplayName("Should determine a cell is present inside a grid")
+    @ParameterizedTest(name = "Determining presence of a cell inside a grid")
+    @CsvSource({
+            "5,5",
+            "6,5",
+            "4,3",
+            "6,6"
+    })
+    public void testToCheckIfACellIsInsideGrid(int row,int col){
+        Grid grid = new Grid(7,7);
+        assertTrue(grid.checkPresence(row, col));
+    }
+
+    @DisplayName("Should determine a cell is present outside a grid")
+    @ParameterizedTest(name = "Determining presence of a cell outside a grid")
+    @CsvSource({
+            "7,7",
+            "8,3",
+            "8,5",
+            "8,1"
+    })
+    public void testToCheckIfACellIsOutsideGrid(int row,int col){
+        Grid grid = new Grid(7,7);
+        assertFalse(grid.checkPresence(row, col));
+    }
+
+    @DisplayName("Calculate the number of live neighbours for the centre cell in a given grid")
+    @ParameterizedTest(name = "Determining number of live neighbours for centre cell")
+    @CsvSource({
+            "7,7",
+            "8,3",
+            "8,5",
+            "8,1"
+    })
+    public void testToCalculateNumberOfLiveNeighboursForCentralCell(int row,int col){
+        Grid grid = new Grid(row,col);
+        assertEquals(8,grid.liveNeighboursForCentre());
+    }
+
+    @DisplayName("Calculate the number of live neighbours for an edge cell in a given grid")
+    @ParameterizedTest(name = "Determining number of live neighbours for an edge cell")
+    @CsvSource({
+            "1,0",
+            "0,1",
+            "2,1",
+            "1,2"
+    })
+    public void testToCalculateNumberOfLiveNeighboursForEdgeCell(int row,int col){
+        Grid grid = new Grid(3,3);
+        assertEquals(5,grid.liveNeighboursForEdgeCell(row,col));
+    }
+
+    @DisplayName("Calculate the number of live neighbours for an corner cell in a given grid")
+    @ParameterizedTest(name = "Determining number of live neighbours for an corner cell")
+    @CsvSource({
+            "0,0",
+            "0,2",
+            "2,0",
+            "2,2"
+    })
+    public void testToCalculateNumberOfLiveNeighboursForCornerCell(int row,int col){
+        Grid grid = new Grid(3,3);
+        assertEquals(3,grid.liveNeighboursForCornerCell(row,col));
+    }
+
+    @DisplayName("Calculate the number of dead neighbours for a corner cell grid")
+    @ParameterizedTest(name = "Determining number of dead neighbours for corner cell")
+    @CsvSource({
+            "0,0",
+            "0,2",
+            "2,0",
+            "2,2"
+    })
+    public void testToCalculateNumberOfDeadNeighboursForCornerCell(int row,int col){
+        Grid grid = new Grid(3,3);
+        assertEquals(4,grid.deadNeighbourCountForBoundaryCells(row,col));
+    }
+
+    @DisplayName("Calculate the number of dead neighbours for an edge cell grid")
+    @ParameterizedTest(name = "Determining number of dead neighbours for edge cell")
+    @CsvSource({
+            "1,0",
+            "0,1",
+            "2,1",
+            "1,2"
+    })
+    public void testToCalculateNumberOfDeadNeighboursForEdgeCell(int row,int col){
+        Grid grid = new Grid(3,3);
+        assertEquals(3,grid.deadNeighbourCountForBoundaryCells(row,col));
+    }
+
+
 }
